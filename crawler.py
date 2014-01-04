de05995834caaf26d91dc998720abbc116cb65df
mao <myj226@gmail.com>
2014-01-04 14:09:35 +0800

    update

--- crawler.py
+++ crawler.py
@@ -21,10 +21,15 @@ def notes(page_url):
     soup = BeautifulSoup(html)
     for note in soup.findAll('div', {'class': 'note-item'}):
         note_url = note.find('a', href = True)
-        print note_url['href']
+        get_note(note_url['href'])
 
-def note(note_url):
-    pass
+def get_note(note_url):
+    html = urllib2.urlopen(note_url).read()
+    soup = BeautifulSoup(html)
+    for note_c in soup.findAll('div', {'class': 'note-content'}):
+        image_url = note_c.find('img', src = True)
+        print image_url['src']
+
 
 def downloads():
     pass
